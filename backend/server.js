16a3f88cece4270a33859be0cb2f6d5080470d7a
Karan <email@karanshergill.com>
2024-12-31 13:40:42 +0530

    update test cases

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve the `index.html` file
+
+// Route for the homepage
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+// Enable serving static files from the frontend directory
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Unconfigured route
+app.get("/open-redirect", (req, res) => {
 });
 
 const PORT = 3000;
